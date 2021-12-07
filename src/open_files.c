c5afe4c89195ea7b47051a5aa321e0a0f7773815
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-12-06 22:21:21 -0300

    fix: file error

--- src/open_files.c
+++ src/open_files.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 18:08:56 by gandrade          #+#    #+#             */
-/*   Updated: 2021/12/05 12:51:29 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/12/06 22:19:58 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 void	open_files(t_vars *vars)
 {
 	vars->infile = open(vars->argv[1], O_RDONLY);
-	if (vars->infile < 0)
-		print_error(NULL);
 	vars->outfile = open(vars->argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (vars->outfile < 0)
+	if (vars->infile < 0 || vars->outfile < 0)
 	{
-		close(vars->infile);
-		vars->infile = 0;
-		print_error(NULL);
+		close_files(vars);
+		print_error("No such file or directory");
 	}
 }
