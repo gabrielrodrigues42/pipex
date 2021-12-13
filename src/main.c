facd078b1a4f7ec526c8794e9bc412de55d19787
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-12-12 22:53:59 -0300

    add: usage example

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 22:22:29 by gandrade          #+#    #+#             */
-/*   Updated: 2021/12/08 17:08:47 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/12/12 22:53:39 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@ int	main(int argc, char **argv, char **envp)
 	{
 		print_error("Too few arguments");
 		print_error("Usage: infile \"cmd1\" \"cmd2\" outfile");
+		print_error("       infile \"ls -la\" \"tr -d ' '\" outfile");
 		exit(1);
 	}
 	else if (argc > 5)
 	{
 		print_error("Too many arguments");
 		print_error("Usage: infile \"cmd1\" \"cmd2\" outfile");
+		print_error("       infile \"ls -la\" \"tr -d ' '\" outfile");
 		exit(1);
 	}
 	else
